86c26f0fee718ee232474469cec8fe8d66517655
Akiyama <mail@msis-net.com>
2024-02-10 10:59:23 +0900

    main

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import relativeLinks from 'astro-relative-links';
 
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +14,11 @@ export default defineConfig({
   },
   server: {
     port: 1234,
-    host: true
+    host: false //ネットワーク全てのif空の接続を許可
   },
-  integrations: [vue(), relativeLinks()]
+  integrations: [vue(), relativeLinks()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
